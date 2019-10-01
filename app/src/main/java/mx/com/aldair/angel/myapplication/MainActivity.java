4f2b4efffa4c5d2e11230e62b5619593ed5c6a19
astat <wapillos2020@gmail.com>
2019-09-30 23:08:37 -0500

    subiendo cambios

--- app/src/main/java/mx/com/aldair/angel/myapplication/MainActivity.java
+++ app/src/main/java/mx/com/aldair/angel/myapplication/MainActivity.java
@@ -3,11 +3,16 @@ package mx.com.aldair.angel.myapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
     }
 }
